eef3288c30e89eef88f8a447d84e43093ff2f7f3
Sammy Schröder <samm0413@elevcampus.dk>
2023-11-28 08:34:12 +0100

    Added a undo move button and changed it so every move is stored in a history

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,24 +1,19 @@
-import { useState } from 'react'
 import Square from './Square'
 import 'bootstrap/dist/css/bootstrap.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRotateLeft } from "@fortawesome/free-solid-svg-icons";
 
-export default function Board() {
-    const [player, setPlayer] = useState('X')
-    const [squares, setSquares] = useState(Array(9).fill(null))
+export default function Board({ player, squares, onPlay, onRestart, onUndo }) {
 
     function handleClick(i) {
+
         if (squares[i] || calculateWinner(squares)) {
             return;
         }
+
         const nextSquares = [...squares]
         nextSquares[i] = player
-        setSquares(nextSquares)
-        setPlayer(player === 'X' ? 'O' : 'X')
-    }
-
-    function handleRestart() {
-        setPlayer('X');
-        setSquares(Array(9).fill(null));
+        onPlay(nextSquares)
     }
 
     const winner = calculateWinner(squares)
@@ -33,51 +28,49 @@ export default function Board() {
         status = player + "'s turn"
     }
 
+
     return (
-        <div className='game'>
-            <h1 className='text-center fw-bold mb-3'>Tic Tac Toe</h1>
-            <div className='d-flex flex-column align-items-center'>
+        <div className='board'>
+            <div className='d-flex flex-column align-items-center mb-5'>
                 <div className='status'>{status}</div>
-                <button className={`restart-button ${winner || squares.every(square => square != null) ? 'pulse-info' : ""}`} onClick={handleRestart}>RESTART</button>
-            </div>
-            <div className='board'>
-                <div className='row'>
-                    <Square value={squares[0]} onSquareClick={() => handleClick(0)} nr="1" />
-                    <Square value={squares[1]} onSquareClick={() => handleClick(1)} nr="2" />
-                    <Square value={squares[2]} onSquareClick={() => handleClick(2)} nr="3" />
-                </div>
-                <div className='row'>
-                    <Square value={squares[3]} onSquareClick={() => handleClick(3)} nr="4" />
-                    <Square value={squares[4]} onSquareClick={() => handleClick(4)} nr="5" />
-                    <Square value={squares[5]} onSquareClick={() => handleClick(5)} nr="6" />
-                </div>
-                <div className='row mb-5'>
-                    <Square value={squares[6]} onSquareClick={() => handleClick(6)} nr="7" />
-                    <Square value={squares[7]} onSquareClick={() => handleClick(7)} nr="8" />
-                    <Square value={squares[8]} onSquareClick={() => handleClick(8)} nr="9" />
+                <div>
+                    <button className={`restart-button me-3 ${winner || squares.every(square => square != null) ? 'pulse-info' : ""}`} onClick={onRestart}>RESTART</button>
+                    <button className='revert-button me-3' onClick={onUndo}><FontAwesomeIcon icon={faArrowRotateLeft} /></button>
                 </div>
             </div>
-            <div className='footer'>&copy; cryze.dev - 2023</div>
+            <div className='playing-field'>
+                {[0, 1, 2].map((row) => (
+                    <div className='row' key={row}>
+                        {[0, 1, 2].map((col) => {
+                            const index = row * 3 + col;
+                            return (
+                                <Square value={squares[index]} onSquareClick={() => handleClick(index)} nr={(row * 3 + col + 1).toString()} key={index} />
+                            )
+                        })}
+                    </div>
+                ))}
+            </div>
         </div>
     )
+
+    function calculateWinner(squares) {
+        const winningLines = [
+            [0, 1, 2], // top row
+            [3, 4, 5], // middle row
+            [6, 7, 8], // bottom row
+            [0, 3, 6], // left column
+            [1, 4, 7], // middle column
+            [2, 5, 8], // right column
+            [0, 4, 8], // diagonal top left to bottom right
+            [2, 4, 6]  // diagonal top right to bottom left
+        ]
+        for (let i = 0; i < winningLines.length; i++) {
+            const [a, b, c] = winningLines[i]
+            if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+                return squares[a]
+            }
+        }
+        return null;
+    }
 }
 
-function calculateWinner(squares) {
-    const winningLines = [
-        [0, 1, 2], // top row
-        [3, 4, 5], // middle row
-        [6, 7, 8], // bottom row
-        [0, 3, 6], // left column
-        [1, 4, 7], // middle column
-        [2, 5, 8], // right column
-        [0, 4, 8], // diagonal top left to bottom right
-        [2, 4, 6]  // diagonal top right to bottom left
-    ]
-    for (let i = 0; i < winningLines.length; i++) {
-        const [a, b, c] = winningLines[i]
-        if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
-            return squares[a]
-        }
-    }
-    return null;
-}
